9c0871353242dd678cd19802104832d208ef205b
AhrooranRavindran <ahrooran17@gmail.com>
2022-05-10 10:11:23 +0530

    added answers -> not complete yet

--- _15.1_exercise_2_ans.sql
+++ _15.1_exercise_2_ans.sql
@@ -1,1 +1,49 @@
-# yet to add answers
+# 1
+select name
+from instructor
+where dept_name = 'Finance'
+order by name asc;
+
+# 2
+select title
+from course
+where dept_name = 'Biology'
+  and credits >= 3
+order by title asc;
+
+# 3
+select name, dept_name, tot_cred
+from student
+where dept_name = 'Comp. Sci.'
+  and tot_cred >= 50
+order by name asc;
+
+# 4
+select distinct instructor.name
+from instructor
+         left join teaches t on instructor.ID = t.ID
+where semester = 'Summer'
+order by name asc;
+
+# 5
+select sum(salary)
+from instructor
+where dept_name = 'Comp. Sci.';
+
+# 6
+select count(id)
+from instructor
+where dept_name = 'Finance';
+
+# 7
+select student.name, tot_cred
+from student
+order by tot_cred desc
+limit 1;
+
+# 8
+select c.course_id, title, year, semester
+from student
+         left join takes t on student.ID = t.ID
+         left join course c on student.dept_name = c.dept_name
+where student.ID = 45678;
--- _17.1_exercise_4_ans.sql
+++ _17.1_exercise_4_ans.sql
@@ -1,1 +1,36 @@
 # yet to add answers
+
+use school;
+
+drop table if exists grade_point;
+
+create table grade_point
+(
+    grade varchar(5) primary key not null,
+    value numeric(2, 1)          not null unique
+);
+
+desc grade_point;
+
+insert into grade_point(grade, value)
+values ('A+', 4.2),
+       ('A', 4),
+       ('A-', 3.7),
+       ('B+', 3.5),
+       ('B', 3),
+       ('B-', 2.7),
+       ('C+', 2.3),
+       ('C', 2.0),
+       ('C-', 1.5),
+       ('D', 1);
+
+select *
+from grade_point
+order by value desc;
+
+#3 -> This solution ignores null values
+select name, sum(value)
+from student
+         natural join takes
+         left join grade_point gp on takes.grade = gp.grade
+group by name;
